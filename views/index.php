1857ef3ed4dfd79d6a04b8549c6a55aa62cad9cd
Jesse Weed <jesse@jesseweed.com>
2013-04-02 21:43:22 -0700

    update main view

--- views/index.php
+++ views/index.php
@@ -1,8 +1,2 @@
-<div class="row">
-    <div class="container mr-t20">
-        <div class="col span_12 mr-b40">
-            <h1 class="mr-b10">Maverick</h1>
-            <p>A super barebones mvc framework for php.</p>
-        </div>
-    </div>
-</div>
+<h1>Maverick</h1>
+<p>A super barebones mvc framework for php.</p>
